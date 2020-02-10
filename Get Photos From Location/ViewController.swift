6d88643d05efcf65d2ffc48f475fc3eafd6b9626
Mohamed <mohammed.korany.ali@gmail.com>
2020-02-11 01:33:32 +0200

    Initial Commit

---
+++ Get Photos From Location/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Get Photos From Location
+//
+//  Created by Mohamed Korany Ali on 2/11/20.
+//  Copyright © 2020 Mohamed Korany Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Get Photos From LocationTests/Get_Photos_From_LocationTests.swift
@@ -0,0 +1,34 @@
+//
+//  Get_Photos_From_LocationTests.swift
+//  Get Photos From LocationTests
+//
+//  Created by Mohamed Korany Ali on 2/11/20.
+//  Copyright © 2020 Mohamed Korany Ali. All rights reserved.
+//
+
+import XCTest
+@testable import Get_Photos_From_Location
+
+class Get_Photos_From_LocationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
